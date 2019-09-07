87e02d42e095c64b49119dc8e5f2f07897e36199
Xiaoteng <616896861@qq.com>
2019-09-07 13:51:49 +0800

    bt一键部署

---
+++ insatll.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# key
+php artisan key:generate
+
+# 软链接
+php artisan storage:link
+
+# 数据表
+php artisan migrate
+
+# 初始化系统权限
+php artisan install role
+
+# 初始化管理员
+php artisan install administrator
+
+# 初始化后台菜单
+php artisan install backend_menu
